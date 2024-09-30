4f5f0c6c5096413013101f183cf804cd0dbccffe
AlexStrizh <strizhkoaa@gmail.com>
2024-10-01 01:03:40 +0300

    01.10 homework1

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ def print_hi(name):
 if __name__ == '__main__':
     print("Hello world")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 1st program
+print(int((9**0.5)*5))
+
+# 2nd program
+print((9,99>9.98) and (1000!=1000.1))
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2==2 * (2 + 2))
+
+# 4th program
+print(int(float('123.456') * 10) % 10)
+
+
