e2431f756aeee40b2829998721c13d50155576d0
AlexStrizh <strizhkoaa@gmail.com>
2024-10-04 01:14:08 +0300

    Переменные

---
+++ variable.py
@@ -0,0 +1,7 @@
+tasks = 'Количество выполненных ДЗ'
+tasks_count = 12
+hours = 'Количество затраченных часов'
+hours_count = 1.5
+course_name = 'Python'
+time_for_one_task = hours_count/tasks_count
+print('Курс:', course_name, ', всего задач:', tasks_count, ', затрачено часов:', hours_count, ', среднее время выполнения', time_for_one_task, 'часа.')
