d8dddd84d78d73987848875468012dc7e515cfbf
AlexStrizh <strizhkoaa@gmail.com>
2024-10-04 02:03:10 +0300

    Переменные (пробелы с преобразованием)

--- variable.py
+++ variable.py
@@ -4,4 +4,4 @@ hours = 'Количество затраченных часов'
 hours_count = 1.5
 course_name = 'Python'
 time_for_one_task = hours_count/tasks_count
-print('Курс: ' + course_name + ', всего задач:', tasks_count, ', затрачено часов:', hours_count, ', среднее время выполнения', time_for_one_task, 'часа.')
+print('Курс: ' + course_name + ', всего задач:' + str(tasks_count) + ', затрачено часов:' + str(hours_count) + ', среднее время выполнения' + str(time_for_one_task) + ' часа.')
